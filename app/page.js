3634f28ccfa445c60a70a057388680de84ff3735
Pawit Kowitanupong <91602142+Eugie123456@users.noreply.github.com>
2023-08-03 14:03:38 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          Math is
+          <code className={styles.code}>not fun</code>
         </p>
         <div>
           <a
